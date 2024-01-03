569451e31b1ad54b5d36dce840e1acca3dc16329
Shatansh Patnaik <shatansh.iitkgp@gmail.com>
2024-01-03 20:58:04 +0530

    Added component files

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import "./index.css";
+
+const route = createBrowserRouter([{
+  path:'/',
+  element: <Layout />,
+  children: [
+    {
+
+    }, {
+
+    }, {
+
+    }, {
+      
+    }
+  ]
+}]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
