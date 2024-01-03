0f892f886a21d912c43399cba57942af19c63253
Shatansh Patnaik <shatansh.iitkgp@gmail.com>
2024-01-03 21:54:24 +0530

    Modified Component files

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import "./index.css";
+import Layout from "./components/Layout.jsx";
+import Home from "./components/Home.jsx";
+import About from "./components/About.jsx";
+import Contact from "./components/Contact.jsx";
+import GitHub from "./components/GitHub.jsx";
 
-const route = createBrowserRouter([{
-  path:'/',
-  element: <Layout />,
-  children: [
-    {
+const route = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+      {
+        path: "github",
+        element: <GitHub />,
+      },
+    ],
+  },
+]);
 
-    }, {
-
-    }, {
-
-    }, {
-      
-    }
-  ]
-}]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={route}></RouterProvider>
+);
